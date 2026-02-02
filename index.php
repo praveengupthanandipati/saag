072d7b9ec89b478c62d63130ceea505f460651fa
Praveen <praveennandipati@gmail.com>
2026-02-02 16:19:11 +0530

    commit changes

--- index.php
+++ index.php
@@ -315,18 +315,56 @@
                             </a>
                         </div>
                     </div>
-
-                    
                 </div>
             </div>
         </section>
 
-       
-         
+        <!-- get in touch -->
+        <section class="getintouch-home">
+            <div class="custom-container">
+                <div class="row">
+                    <div class="col-md-4">
+                         <div class="title-section">                           
+                            <h2 class="font-bold font-white">Let's Create Something Infinite.</h2>
+                             <h5 class="font-white">Whether you're here to learn, build, create, or collaborate.</h5>
+                             <a href="javascript:void(0)" class="primary-btn mt-3">Get in Touch Today</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>   
     </main>
 
     <!-- footer -->
-    <footer></footer>
+    <footer>
+        <section class="custom-container">
+            <div class="row">
+                <div class="col-md-6">
+                     <div class="title-section">                           
+                            <h2 class="font-bold"><span class="font-primary">We are always</span> happy to talk! </h2>
+                           <div class="footer-social">
+                             <a href="javascript:void(0)" target="_blank" class="font-bold">Facebook</a>
+                            <a href="javascript:void(0)" target="_blank" class="font-bold">Twitter</a>
+                            <a href="javascript:void(0)" target="_blank" class="font-bold">Instagram</a>
+                            <a href="javascript:void(0)" target="_blank" class="font-bold">Linkedin</a>
+                           </div>  
+                        </div>
+                </div>
+            </div>
+
+            <div class="row pt-3 pt-md-5">
+                <div class="col-md-4">
+                    <p class="font-semibold">Door no. 1-98/8/23/SSN/A/102, Sri Sai Estates, <br>Vital rao nagar, Madhapur, Hyderabad</p>
+                </div>
+                <div class="col-md-4">                    
+                    <p class="pb-0 m-0 font-semibold"><a href="call to:+91 95425 20797">+91 95425 20797</a></p>
+                    <p class="font-semibold"><a href="mailto:saaginfinitecreatives@gmail.com">saaginfinitecreatives@gmail.com</a></p>
+                </div>
+                <div class="col-md-4">
+                    <p class="font-semibold">© 2026 by Saag Infinite Creatives. </p>
+                </div>
+        </section>
+    </footer>
     <!-- scripts -->
     <!--[if lte IE 9]>
       <script src="js/ie.lteIE9.js"></script>
